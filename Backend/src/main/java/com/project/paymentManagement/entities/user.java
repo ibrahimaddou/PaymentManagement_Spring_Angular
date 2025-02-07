9b2c2fad26349c06484303fff011b187d83e592d
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-02-07 15:58:19 +0100

    création de l'entité user

---
+++ Backend/src/main/java/com/project/paymentManagement/entities/user.java
@@ -0,0 +1,18 @@
+package com.project.paymentManagement.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter @ToString @Builder
+public class user {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String code;
+    private String programmeId;
+    private String photo;
+
+}
