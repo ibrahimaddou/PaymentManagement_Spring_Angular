8db946153caeeeb00ff50b39e0c8ae8124ce66fe
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-02-27 16:53:48 +0100

    configuration de Services : mettre les services dans Services et les injecter dans le controleur

--- Backend/src/main/java/com/project/paymentManagement/Web/PaymentRestController.java
+++ Backend/src/main/java/com/project/paymentManagement/Web/PaymentRestController.java
@@ -3,6 +3,7 @@ package com.project.paymentManagement.Web;
 import com.project.paymentManagement.Repository.ClientRepository;
 import com.project.paymentManagement.Repository.PaymentRepository;
 import com.project.paymentManagement.Repository.ClientRepository;
+import com.project.paymentManagement.Services.PaymentService;
 import com.project.paymentManagement.entities.Payment;
 import com.project.paymentManagement.entities.PaymentStatus;
 import com.project.paymentManagement.entities.PaymentType;
@@ -27,10 +28,11 @@ import java.util.UUID;
 public class PaymentRestController {
     private ClientRepository clientRepository;
     private PaymentRepository paymentRepository;
-
+    private PaymentService paymentService;
     public PaymentRestController(PaymentRepository paymentRepository,ClientRepository  clientRepository) {
         this.clientRepository = clientRepository;
         this.paymentRepository = paymentRepository;
+        this.paymentService = paymentService;
     }
     @GetMapping(path ="/payments")
     public List<Payment> allPayments(){
@@ -67,28 +69,16 @@ public class PaymentRestController {
     }
     @PutMapping("/payments/{id}") //HTTP PUT requests is to modify a ressource
     public Payment updatePaymentStatus(@RequestParam PaymentStatus status, @PathVariable Long id){
-        Payment payment = paymentRepository.findById(id).get();
-        payment.setStatus(status);
-        return paymentRepository.save(payment);
+
+        return paymentService.updatePaymentStatus(status,id);
     }
 
     @PostMapping(path="/payments", consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
     public Payment savePayment(@RequestParam MultipartFile file, LocalDate date, double amount, PaymentType type, String clientCode)throws IOException {
-        Path folderPath = Paths.get(System.getProperty("user.home"),"pay-data","payments");
-        if(!Files.exists(folderPath)){
-            Files.createDirectories(folderPath);
-        }
-        String fileName =UUID.randomUUID().toString();
-        Path filePath = Paths.get(System.getProperty("user.home"),"pay-data","payments",fileName+".pdf");
-        Files.copy(file.getInputStream(),filePath);
-        Client client = clientRepository.findByCode(clientCode);
-        Payment payment = Payment.builder()
-            .date(date).type(type).client(client)
-            .amount(amount)
-            .file(filePath.toUri().toString())
-            .status(PaymentStatus.CREATED)
-            .build();
-        return paymentRepository.save(payment);
+        return this.paymentService.savePayment(file,date,amount,type,clientCode);
     }
-
+    @GetMapping(path="/paymentFile/{paymentId}",produces = MediaType.APPLICATION_PDF_VALUE)
+    public byte[] getPaymentFile( Long paymentId) throws IOException {
+        return this.paymentService.getPaymentFile(paymentId);
+    }
 }
