7958f0076efdd2890528bd0b79d7209d9ed8fa4a
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-02-28 15:54:55 +0100

    creation des components essentiel pour l'App

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -11,12 +11,30 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoadClientsComponent } from './load-clients/load-clients.component';
+import { LoadPaymentsComponent } from './load-payments/load-payments.component';
+import { LoginComponent } from './login/login.component';
+import { StudentsComponent } from './students/students.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminTemplateComponent
+    AdminTemplateComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoadClientsComponent,
+    LoadPaymentsComponent,
+    LoginComponent,
+    StudentsComponent,
+    ClientsComponent,
+    PaymentsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
