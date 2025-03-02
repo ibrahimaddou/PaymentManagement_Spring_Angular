7b8cf42c1faca732adfd1da41a22859fdf9a7ac7
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-03-02 11:52:25 +0100

    creation of the login form

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
-  constructor(){
+  public loginForm! : FormGroup;
+
+  constructor(private fb :FormBuilder){
 
   }
   ngOnInit(): void {
-      
+      this.loginForm =this.fb.group({
+        username : this.fb.control(''),
+        password : this.fb.control('')
+      });
   }
 }
