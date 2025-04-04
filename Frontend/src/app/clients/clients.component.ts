cc256f147d9ec25e03cc1a7a34a6e0efa8a48246
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-04-04 19:56:55 +0200

    getClients() to get the clients from the backend

--- Frontend/src/app/clients/clients.component.ts
+++ Frontend/src/app/clients/clients.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientsService } from '../services/clients.service';
+import { Client, Payment } from '../model/clients.model';
+
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css'
 })
-export class ClientsComponent {
+export class ClientsComponent  implements OnInit{
+  clients! : Array<Client>;
+  constructor(private clientsService : ClientsService){
+      
+    }
+    ngOnInit(): void {
+      this.clientsService.getClients()
+      .subscribe({
+        next: (value: Client[]) => {  
+          this.clients = value;
+        },
+        error : err =>{
+          console.log(err);
+        }
+      })
+    }
+  
+    
+  
+  
 
 }
--- Frontend/src/app/services/clients.service.ts
+++ Frontend/src/app/services/clients.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
-import { Payment } from '../model/clients.model.model';
+import { Client, Payment } from '../model/clients.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +13,9 @@ export class ClientsService {
  
   public getAllPayments():Observable<Array<Payment>>{
     return this.http.get<Array<Payment>>(`${environment.backendHost}/payments`); 
+  }
+
+  public getClients():Observable<Array<Client>>{
+    return this.http.get<Array<Client>>(`${environment.backendHost}/clients`); 
   }
 }
