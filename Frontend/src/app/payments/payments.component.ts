88d688268f0c8b537a0ca48eb81d5a14240de669
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-04-04 19:31:12 +0200

    ClientsService injection

--- Frontend/src/app/payments/payments.component.ts
+++ Frontend/src/app/payments/payments.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, viewChild, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { ClientsService } from '../services/clients.service';
 
 @Component({
   selector: 'app-payments',
@@ -18,11 +19,11 @@ export class PaymentsComponent implements OnInit {
    @ViewChild(MatSort)sort!: MatSort;
 
 
-  constructor(private http : HttpClient){
+  constructor(private clientsService : ClientsService){
     
   }
   ngOnInit(): void {
-    this.http.get("http://localhost:8021/payments")
+    this.clientsService.getAllPayments()
     .subscribe({
       next : data =>{
         this.payments = data; //getting data
--- Frontend/src/app/services/clients.service.ts
+++ Frontend/src/app/services/clients.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
+import { Payment } from '../model/clients.model.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ClientsService {
 
   constructor(private http :HttpClient) { }
  
-  public getAllPayments():Observable<any>{
-    return this.http.get(`${environment.backendHost}/payments`); 
+  public getAllPayments():Observable<Array<Payment>>{
+    return this.http.get<Array<Payment>>(`${environment.backendHost}/payments`); 
   }
 }
